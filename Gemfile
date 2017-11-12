6a4c2a432727dc6f9602f620ec678f9a7905c16f
Slava <slava.marushhak@mail.ru>
2017-11-12 11:15:24 +0200

    add pg for production

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'devise'
 gem 'pg'
 #
 gem 'bootstrap-sass'
+#PaperClip for avatar
+gem "paperclip", "~> 5.0.0"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-    # This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103124932) do
+ActiveRecord::Schema.define(version: 20171110090131) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -73,6 +73,10 @@ ActiveRecord::Schema.define(version: 20171103124932) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
